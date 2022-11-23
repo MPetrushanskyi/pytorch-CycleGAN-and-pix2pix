ebfd153d57b3425f8f166d0ed99e89c006b31756
Mykhailo Petrushanskyi <mpetrushanskyi@gmail.com>
2022-11-23 11:33:04 +0200

    fix

--- train.py
+++ train.py
@@ -23,7 +23,7 @@ from options.train_options import TrainOptions
 from data import create_dataset
 from models import create_model
 from util.visualizer import Visualizer
-from torchsummary import summary
+from torchinfo import summary
 
 if __name__ == '__main__':
     opt = TrainOptions().parse()   # get training options
