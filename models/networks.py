88d358529ed2899bdd71fde322f5d6f18c956666
Mykhailo Petrushanskyi <mpetrushanskyi@gmail.com>
2022-11-23 11:11:43 +0200

    Conv2dSame

--- models/networks.py
+++ models/networks.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 from torch.nn import init
 import functools
 from torch.optim import lr_scheduler
+import torch.nn.functional as F
+
 
 
 ###############################################################################
@@ -503,6 +505,30 @@ class AutoRetouchBlock(nn.Module):
         """Forward function (with skip connections)"""
         out = x + self.conv_block(x)  # add skip connections
         return out
+class Conv2dSame(torch.nn.Conv2d):
+
+    def calc_same_pad(self, i: int, k: int, s: int, d: int) -> int:
+        return max((math.ceil(i / s) - 1) * s + (k - 1) * d + 1 - i, 0)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        ih, iw = x.size()[-2:]
+
+        pad_h = self.calc_same_pad(i=ih, k=self.kernel_size[0], s=self.stride[0], d=self.dilation[0])
+        pad_w = self.calc_same_pad(i=iw, k=self.kernel_size[1], s=self.stride[1], d=self.dilation[1])
+
+        if pad_h > 0 or pad_w > 0:
+            x = F.pad(
+                x, [pad_w // 2, pad_w - pad_w // 2, pad_h // 2, pad_h - pad_h // 2]
+            )
+        return F.conv2d(
+            x,
+            self.weight,
+            self.bias,
+            self.stride,
+            self.padding,
+            self.dilation,
+            self.groups,
+        )
 
 class AutoRetouchInnerBlock(nn.Module):
     """Define an inner block"""
@@ -516,7 +542,8 @@ class AutoRetouchInnerBlock(nn.Module):
         
         inner_block = []
         
-        inner_block+=[nn.Conv2d(64, 128, kernel_size=7, stride=2, padding="same")  ]
+        #inner_block+=[nn.Conv2d(64, 128, kernel_size=7, stride=2, padding=)  ]
+        inner_block+=[Conv2dSame(64, 128, kernel_size=7, stride=2)  ]
         n_blocks=15
         for i in range(n_blocks):       # add ResNet blocks
 
